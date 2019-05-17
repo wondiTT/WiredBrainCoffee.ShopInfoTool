75a5e16c3eff28c572e806e07dd653922e781494
Wondi Tanessob <wondi.tanessob@jungdms.de>
2019-05-17 11:07:55 +0200

    D - Implement help command

--- WiredBrainCoffee.ShopInfoTool/Program.cs
+++ WiredBrainCoffee.ShopInfoTool/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using WiredBrainCoffee.DataAccess;
 
 namespace WiredBrainCoffee.ShopInfoTool
 {
@@ -7,6 +8,23 @@ namespace WiredBrainCoffee.ShopInfoTool
         static void Main(string[] args)
         {
             Console.WriteLine("Wired Brain Coffee - Shop Info Tool!");
+            Console.WriteLine("Write 'help' to list available commands");
+
+            var coffeeShopDataprovider = new CoffeeShopDataProvider();
+
+            while (true)
+            {
+                var line = Console.ReadLine();
+                var coffeeShops = coffeeShopDataprovider.LoadCoffeeeShops();
+                if (string.Equals("help", line, StringComparison.OrdinalIgnoreCase))
+                {
+                    Console.WriteLine("> Available coffee shop commands:");
+                    foreach(var coffeeShop in coffeeShops)
+                    {
+                        Console.WriteLine($"> " + coffeeShop.Location);
+                    }
+                }
+            }
         }
     }
 }
