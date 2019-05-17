c025562406339fac59c01288b98b9c89f46f8e8e
Julia Marschall <wonditeamdeveloper@gmail.com>
2019-05-17 12:52:46 +0200

    I - Add PaperCupInStock property (Branch2)

--- WiredBrainCoffee.DataAccess/CoffeeShopDataProvider.cs
+++ WiredBrainCoffee.DataAccess/CoffeeShopDataProvider.cs
@@ -6,9 +6,9 @@ namespace WiredBrainCoffee.DataAccess
     {
         public IEnumerable<CoffeeShop> LoadCoffeeeShops()
         {
-            yield return new CoffeeShop { Location = "Munich", BeansInStockInKg = 56 };
-            yield return new CoffeeShop { Location = "Freiburg", BeansInStockInKg = 23 };
-            yield return new CoffeeShop { Location = "Frankfurt", BeansInStockInKg = 107 };
+            yield return new CoffeeShop { Location = "Munich", BeansInStockInKg = 56, PaperCupsInStock=350 };
+            yield return new CoffeeShop { Location = "Freiburg", BeansInStockInKg = 23, PaperCupsInStock=250 };
+            yield return new CoffeeShop { Location = "Frankfurt", BeansInStockInKg = 107, PaperCupsInStock=427 };
 
         }
     }
--- WiredBrainCoffee.DataAccess/Model/CoffeeShop.cs
+++ WiredBrainCoffee.DataAccess/Model/CoffeeShop.cs
@@ -4,5 +4,6 @@
     {
         public string Location { get; set; }
         public int BeansInStockInKg { get; set; }
+        public int PaperCupsInStock { get; set; }
     }
 }
