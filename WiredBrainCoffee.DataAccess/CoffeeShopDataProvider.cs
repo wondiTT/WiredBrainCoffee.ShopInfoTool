14bb357f39b18db6625c924da008ad11e998d51d
Wondi Tanessob <wondi.tanessob@jungdms.de>
2019-05-17 11:07:22 +0200

    C - Add data access project to solution

---
+++ WiredBrainCoffee.DataAccess/CoffeeShopDataProvider.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace WiredBrainCoffee.DataAccess
+{
+    public class CoffeeShopDataProvider
+    {
+        public IEnumerable<CoffeeShop> LoadCoffeeeShops()
+        {
+            yield return new CoffeeShop { Location = "Munich", BeansInStockInKg = 56 };
+            yield return new CoffeeShop { Location = "Freiburg", BeansInStockInKg = 23 };
+            yield return new CoffeeShop { Location = "Frankfurt", BeansInStockInKg = 107 };
+
+        }
+    }
+}
---
+++ WiredBrainCoffee.DataAccess/Model/CoffeeShop.cs
@@ -0,0 +1,8 @@
+﻿namespace WiredBrainCoffee.DataAccess
+{
+    public class CoffeeShop
+    {
+        public string Location { get; set; }
+        public int BeansInStockInKg { get; set; }
+    }
+}
